a9019a7e3ee4dff560f39018317383f8a8bd92a2
ggalazzo <gabor.galazzo@gmail.com>
2022-10-28 14:23:21 +0200

    fix chiamate remote pendenti, cambta vista dei ranch

--- event-rails-server/src/main/java/Test.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-
-public class Test {
-	public static void main(String[] args) {
-		var textArea = new JTextArea(5, 20);
-		JScrollPane scrollPane = new JScrollPane(textArea);
-		textArea.setEditable(false);
-	}
-}
