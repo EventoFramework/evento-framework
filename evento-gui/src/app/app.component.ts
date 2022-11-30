3f723096125477f2164365d92cd1c044d2a0c492
Gabor Galazzo <gabor.galazzo@gmail.com>
2022-11-30 19:00:39 +0100

    flow refactoring

--- evento-gui/src/app/app.component.ts
+++ evento-gui/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   public appPages = [
     { title: 'application.map.title', url: '/application-map', icon: 'apps' },
-    { title: 'application.flows.title', url: '/application-flows', icon: 'git-compare' },
+    { title: 'application.flows.title', url: '/application-flows/all', icon: 'git-compare' },
     { title: 'cluster.status', url: '/cluster-status', icon: 'layers' },
     { title: 'bundle.list.title', url: '/bundle-list', icon: 'cube' },
     { title: 'library.title', url: '/library-page', icon: 'book' },
