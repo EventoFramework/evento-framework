01d777e405e5050d182e64efa73af374ef7e166b
Gabor Galazzo <gabor.galazzo@gmail.com>
2022-12-01 22:00:43 +0100

    flow fix order and async

--- evento-gui/src/app/pages/application-flows/application-flows-page.component.ts
+++ evento-gui/src/app/pages/application-flows/application-flows-page.component.ts
@@ -1,11 +1,10 @@
 import {Component, OnInit} from '@angular/core';
-import {HandlerService} from "../../services/handler.service";
-import {BundleColorService} from "../../services/bundle-color.service";
-import {add} from "ionicons/icons";
+import {HandlerService} from '../../services/handler.service';
+import {BundleColorService} from '../../services/bundle-color.service';
 import * as mermaid from 'mermaid';
 
-declare var mxGraph: any;
-declare var mxHierarchicalLayout: any;
+declare let mxGraph: any;
+declare let mxHierarchicalLayout: any;
 
 @Component({
   selector: 'app-application-petri-net',
@@ -24,132 +23,12 @@ export class ApplicationFlowsPage implements OnInit {
   async ngOnInit() {
 
 
-    const handlers = await this.handlerService.findAll();
-    /*
-    var handler = handlers.filter(h => h.handledPayload.name === 'DemoLifecycleAgent::action')[0];
-    console.log(handler);
-
-
-
-
-    /*
-    const network = await this.handlerService.getPetriNet();
-    const posts = {};
-    for(const p of network.posts){
-      posts[p.id] = p;
-    }
-    const transitions = {};
-    for(const t of network.transitions){
-      transitions[t.id] = t;
-    }
-    var handler = handlers.filter(h => h.handledPayload.name === 'DemoLifecycleAgent::action')[0];
-    var diagram = "sequenceDiagram\n";
-    const manageTransition = (c, t) => {
-      console.log("t", t)
-      const arrow = c == "EventStore" ? "-->>" : "->>"
-      diagram += c+arrow+t.component+": "+t.action+"\n";
-      for(const tt of t.target){
-        console.log("p", posts[tt])
-        if(posts[tt].action === 'Token') {
-          continue;
-        }
-        if(posts[tt].action === 'Sink') {
-          continue;
-        }
-        if(posts[tt].component === 'ProjectorStore') {
-          continue;
-        }
-        if(posts[tt].component === 'SagaStore') {
-          continue;
-        }
-        if(posts[tt].component === t.component && posts[tt].action === t.action){
-          continue;
-        }
-        for(const pt of posts[tt].target){
-          if(transitions[pt].component == 'Gateway'){
-            for(const gp of transitions[pt].target){
-              if(posts[gp].action === 'Token') {
-                continue;
-              }
-              if(posts[gp].action === 'Sink') {
-                continue;
-              }
-              if(posts[gp].component === 'ProjectorStore') {
-                continue;
-              }
-              if(posts[gp].component === 'SagaStore') {
-                continue;
-              }
-              if(posts[gp].component === t.component && posts[gp].action === t.action){
-                continue;
-              }
-              for(const gpt of posts[gp].target){
-                manageTransition(t.component, transitions[gpt]);
-              }
-            }
-          }else {
-            manageTransition(t.component, transitions[pt]);
-          }
-        }
-      }
-    }
-
-    const t = network.transitions.filter(t => t.bundle == handler.bundleId && t.component == handler.componentName && t.action == handler.handledPayload.name)[0];
-
-    manageTransition("Invoker", t);
-
-    mermaid.default.initialize({});
-    const container = <HTMLElement>document.getElementById('graph');
-    console.log(diagram);
-    mermaid.default.render("graphDiv", diagram, (svgCode) => {
-      container.innerHTML = svgCode;
-    });
-
-
-
-
-
-    /*
-    const generateSequence = (handler, parentComponent, previous, parent, sync) => {
-
-      for(const i of handler.invocations){
-        for(const h of handlers.filter(h => h.handledPayload.name === i.name)){
-          previous += handler.componentName + "->>" + h.componentName + ": " + h.handledPayload.name + "\n"
-          previous += generateSequence(h, handler.componentName, "", () => h.componentName + "->>" + handler.componentName + ": \n" +(sync ? handler.componentName + "->>" + parentComponent + ": \n" +  parent() : ""), true)
-        }
-      }
-      if(["InvocationHandler", "CommandHandler", "AggregateCommandHandler", "ServiceCommandHandler", "QueryHandler"].includes(handler.handlerType) && sync) {
-       previous += parent();
-      }
-      if(handler.returnType){
-        for(const h of handlers.filter(h => h.handledPayload.name === handler.returnType.name && h.handlerType !== 'EventSourcingHandler')){
-          previous += handler.componentName + "-->>" + h.componentName + ": " + h.handledPayload.name + "\n"
-          previous += generateSequence(h, handler.componentName, "",() => "", false)
-        }
-      }
-
-      return previous;
-    }
-
-
-    var handler = handlers.filter(h => h.handledPayload.name === 'DemoLifecycleAgent::action')[0];
-    console.log(handler);
-    const container = <HTMLElement>document.getElementById('graph');
-    mermaid.default.initialize({});
-    let graphDefinition = generateSequence(handler,"Invoker", "sequenceDiagram\nInvoker->>" + handler.componentName + ": " + handler.handledPayload.name + "\n",
-      () => "", true)
-    console.log(graphDefinition);
-    mermaid.default.render("graphDiv", graphDefinition, (svgCode) => {
-      container.innerHTML = svgCode;
-    });
-    /*
-*/
         const network = await this.handlerService.getPetriNet();
         const container = <HTMLElement>document.getElementById('graph');
 
-    const graph = new mxGraph(container)
-    const parent = graph.getDefaultParent()
-    graph.setTooltips(true)
+    const graph = new mxGraph(container);
+    const parent = graph.getDefaultParent();
+    graph.setTooltips(true);
 
     // Enables panning with left mouse button
     graph.panningHandler.useLeftButtonForPanning = true;
@@ -168,38 +47,38 @@ export class ApplicationFlowsPage implements OnInit {
           graph.zoomOut();
         }
       }
-    })
+    });
 
     const showPosts = false;
 
-    var layout = new mxHierarchicalLayout(graph, 'north');
-    graph.getModel().beginUpdate()
+    const layout = new mxHierarchicalLayout(graph, 'west');
+    graph.getModel().beginUpdate();
     try {
-      const postNodes = {}
-      const transitionNodes = {}
-      const transitionRef = {}
-      const postStyle = "shape=ellipse;whiteSpace=wrap;perimeter=ellipsePerimeter;strokeColor=grey;fontColor=black;fillColor=transparent"
+      const postNodes = {};
+      const transitionNodes = {};
+      const transitionRef = {};
+      const postStyle = 'shape=ellipse;whiteSpace=wrap;perimeter=ellipsePerimeter;strokeColor=grey;fontColor=black;fillColor=transparent';
       if (showPosts) {
         for (const post of network.posts) {
           postNodes[post.id] = graph.insertVertex(parent, post.id, null, null, null, 50, 50, postStyle);
         }
       }
-      const transitionStyle = "shape=rectangle;whiteSpace=wrap;perimeter=ellipsePerimeter;strokeColor=grey;fontColor=black;"
+      const transitionStyle = 'shape=rectangle;whiteSpace=wrap;perimeter=ellipsePerimeter;strokeColor=grey;fontColor=black;';
       for (const transition of network.transitions) {
-        if (transition.action === 'Sink') continue;
-        transition.name = transition.bundle + "\n\n" + transition.component + "\n\n" + transition.action
-        Math.max(transition.bundle.length, transition.component.length, transition.action.length)
-        let additionalStyles = "fillColor=" + this.bundleColorService.getColorForBundle(transition.bundle) + ";"
-        let width = Math.max(transition.bundle.length, transition.component.length, transition.action.length) * 10;
+        if (transition.action === 'Sink') {continue;}
+        transition.name = transition.bundle + '\n\n' + transition.component + '\n\n' + transition.action;
+        Math.max(transition.bundle.length, transition.component.length, transition.action.length);
+        let additionalStyles = 'fillColor=' + this.bundleColorService.getColorForBundle(transition.bundle) + ';';
+        const width = Math.max(transition.bundle.length, transition.component.length, transition.action.length) * 10;
         let height = 90;
         let text = transition.name;
         if (transition.meanServiceTime) {
-          text += "\n\nmst: " + transition.meanServiceTime.toFixed(3) + " [ms]"
-          height += 30
+          text += '\n\nmst: ' + transition.meanServiceTime.toFixed(3) + ' [ms]';
+          height += 30;
         }
         if (transition.bundle === 'event-store' || transition.component === 'SagaStore' || transition.component === 'ProjectorStore') {
-          additionalStyles += "shape=cylinder;verticalAlign=bottom;spacingBottom=20;"
-          height += 70
+          additionalStyles += 'shape=cylinder;verticalAlign=bottom;spacingBottom=20;';
+          height += 70;
         }
         transitionNodes[transition.id] = graph.insertVertex(parent, transition.id, text, null, null, width,
           height,
@@ -215,27 +94,35 @@ export class ApplicationFlowsPage implements OnInit {
         }
       }
 
-      const edgeStyle = "endArrow=classic;html=1;rounded=0;curved=1;labelBackgroundColor=white"
+      const edgeStyle = 'endArrow=classic;html=1;rounded=0;curved=1;labelBackgroundColor=white';
       for (const transition of network.transitions) {
-        for (const target of transition.target) {
-          if (showPosts) {
-            graph.insertEdge(parent, null, '', transitionNodes[transition.id], postNodes[target], edgeStyle);
-          } else {
+        if (showPosts) {
+          for (const target of transition.target) {
+              graph.insertEdge(parent, null, '', transitionNodes[transition.id], postNodes[target], edgeStyle);
+            }
+        }else{
+          const transitions = [];
+          for (const target of transition.target) {
             const targetTransition = network.posts.filter(p => p.id === target && p.action !== 'Token')[0];
             if (targetTransition) {
               for (const targetTarget of targetTransition.target) {
                 if (transition.id !== targetTarget && transitionNodes[targetTarget]) {
-                  const target  = transitionRef[targetTarget]
-                  var txt = ''
-                  if(target.meanServiceTime && target.meanThroughput){
-                    txt = (target.meanServiceTime*target.meanThroughput).toFixed(3) +" [r]"
+                  transitions.push(transitionRef[targetTarget]);
                   }
-                  graph.insertEdge(parent, null, txt, transitionNodes[transition.id], transitionNodes[targetTarget], edgeStyle);
-                }
               }
             }
+
+          }
+          for(const target of transitions.sort((a,b) => a?.async - b?.async)){
+            let txt = '';
+            if(target.meanServiceTime && target.meanThroughput){
+              txt = (target.meanServiceTime*target.meanThroughput).toFixed(3) +' [r]';
+            }
+            graph.insertEdge(parent, null, txt, transitionNodes[transition.id],
+              transitionNodes[target.id], edgeStyle +';'+ (target.async ? 'dashed=1' : 'dashed=0'));
           }
         }
+
       }
 
       // Executes the layout
@@ -243,7 +130,7 @@ export class ApplicationFlowsPage implements OnInit {
 
 
     } finally {
-      graph.getModel().endUpdate()
+      graph.getModel().endUpdate();
     }
   }
 
