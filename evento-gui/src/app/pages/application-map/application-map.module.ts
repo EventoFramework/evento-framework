221e8e2407b7e7526a84fdf81919464e1110a0eb
Gabor Galazzo <gabor.galazzo@gmail.com>
2023-03-12 14:11:22 +0100

    Flow Visualization OK

--- evento-gui/src/app/pages/application-map/application-map.module.ts
+++ evento-gui/src/app/pages/application-map/application-map.module.ts
@@ -8,15 +8,17 @@ import { ApplicationMapPageRoutingModule } from './application-map-routing.modul
 
 import { ApplicationMapPage } from './application-map.page';
 import {TranslateModule} from "@ngx-translate/core";
+import {ComponentsModule} from "../../components/components.module";
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    ApplicationMapPageRoutingModule,
-    TranslateModule
-  ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        ApplicationMapPageRoutingModule,
+        TranslateModule,
+        ComponentsModule
+    ],
     declarations: [ApplicationMapPage]
 })
 export class ApplicationMapPageModule {}
