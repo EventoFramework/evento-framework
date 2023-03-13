7d37859d2042c4bbaebd1b6cf51533b778ad1bc7
Gabor Galazzo <gabor.galazzo@gmail.com>
2023-03-13 20:13:26 +0100

    fix for indexes

--- evento-server/src/main/resources/schema.sql
+++ evento-server/src/main/resources/schema.sql
@@ -1,8 +1,24 @@
-CREATE TABLE IF NOT EXISTS int_lock  (
-						   lock_key CHAR(36) NOT NULL,
-						   region VARCHAR(100) NOT NULL,
-						   client_id CHAR(36),
-						   created_date DATETIME(6) NOT NULL,
-						   constraint INT_LOCK_PK primary key (lock_key, region)
+CREATE TABLE IF NOT EXISTS `int_lock` (
+							`lock_key` char(36) NOT NULL,
+							`region` varchar(100) NOT NULL,
+							`client_id` char(36) DEFAULT NULL,
+							`created_date` datetime(6) NOT NULL,
+							PRIMARY KEY (`lock_key`,`region`)
 ) ENGINE=InnoDB;
+
+
+
+CREATE TABLE IF NOT EXISTS `es__events` (
+							  `event_sequence_number` bigint NOT NULL,
+							  `aggregate_id` varchar(255) DEFAULT NULL,
+							  `created_at` bigint NOT NULL,
+							  `event_message` blob NOT NULL,
+							  `event_name` varchar(255) NOT NULL,
+							  KEY `es__events_aggregate_id_event_sequence_number_index` (`aggregate_id`,`event_sequence_number`),
+							  KEY `es__events_event_name_event_sequence_number_index` (`event_name`,`event_sequence_number`),
+							  KEY `es__events_event_sequence_number_index` (`event_sequence_number`)
+) ENGINE=InnoDB;
+
+
+
 
