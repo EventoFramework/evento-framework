bba517f04a5f0678a9514d507ef40e386bd6d228
ggalazzo <gabor.galazzo@gmail.com>
2023-03-23 19:30:59 +0100

    dashboard iniziata

--- evento-gui/src/app/pages/dashboard/dashboard.page.ts
+++ evento-gui/src/app/pages/dashboard/dashboard.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DashboardService} from "../../services/dashboard.service";
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.page.scss'],
 })
 export class DashboardPage implements OnInit {
+  dashboard: any;
 
-  constructor() { }
+  constructor(private dashboardService: DashboardService) {
+  }
 
   ngOnInit() {
+  }
+
+  async ionViewWillEnter() {
+    this.dashboard = await this.dashboardService.getDashboard();
+    console.log(this.dashboard);
   }
 
 }
