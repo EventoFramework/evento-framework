202ad5b070341351673c005825869e3ca53e2f0b
Gabor Galazzo <gabor.galazzo@gmail.com>
2023-03-27 00:50:12 +0200

    Aggiunto componente con saga ricorsiva Fixata formula

---
+++ evento-demo/evento-demo-pc/src/main/java/org/evento/demo/pc/api/PcEvent1.java
@@ -0,0 +1,17 @@
+package org.evento.demo.pc.api;
+
+import org.evento.common.modeling.messaging.payload.DomainCommand;
+import org.evento.common.modeling.messaging.payload.DomainEvent;
+
+public class PcEvent1 extends DomainEvent {
+
+	private String pcId;
+
+	public String getPcId() {
+		return pcId;
+	}
+
+	public void setPcId(String pcId) {
+		this.pcId = pcId;
+	}
+}
---
+++ evento-demo/evento-demo-pc/src/test/java/org/evento/demo/pc/EventoDemoPcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.evento.demo.pc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventoDemoPcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
