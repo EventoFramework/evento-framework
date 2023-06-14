d945e12da03b7c881ee4625e6e2339beea510dc2
Gabor Galazzo <gabor.galazzo@gmail.com>
2023-06-14 20:10:00 +0200

    Telemetry skeleton to message gateway level (not directly implemented)

--- evento-common/src/main/java/org/evento/common/messaging/gateway/QueryGateway.java
+++ evento-common/src/main/java/org/evento/common/messaging/gateway/QueryGateway.java
@@ -1,5 +1,6 @@
 package org.evento.common.messaging.gateway;
 
+import org.evento.common.modeling.messaging.message.application.Message;
 import org.evento.common.modeling.messaging.payload.Query;
 import org.evento.common.modeling.messaging.query.QueryResponse;
 
@@ -11,5 +12,8 @@ public interface QueryGateway {
 	default <T extends QueryResponse<?>> CompletableFuture<T> query(Query<T> query){
 		return query(query, null);
 	}
-	<T extends QueryResponse<?>> CompletableFuture<T> query(Query<T> query, HashMap<String, String> metadata);
+	default <T extends QueryResponse<?>> CompletableFuture<T> query(Query<T> query, HashMap<String, String> metadata){
+		return query(query, metadata, null);
+	}
+	<T extends QueryResponse<?>> CompletableFuture<T> query(Query<T> query, HashMap<String, String> metadata, Message<?> handledMessage);
 }
