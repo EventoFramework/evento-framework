05b95d521b21cdaeedf45009a7bf287509614040
Gabor Galazzo <gabor.galazzo@gmail.com>
2023-06-17 02:21:03 +0200

    entri tracing working, to enrich with other data

---
+++ evento-bundle/src/main/java/org/evento/application/performance/TracingAgent.java
@@ -0,0 +1,28 @@
+package org.evento.application.performance;
+
+import org.evento.application.reference.Reference;
+import org.evento.common.modeling.messaging.message.application.Message;
+
+import java.util.HashMap;
+import java.util.Objects;
+
+public interface TracingAgent {
+
+	public static interface Transaction<T>{
+		public T run() throws Throwable;
+	}
+
+	public default void correlate(Message<?> from, Message<?> to){
+		to.setMetadata(correlate(to.getMetadata(), from));
+	}
+
+	default   <T> T  track(Message<?> message, String component, String bundle, long bundleVersion, Transaction<T> transaction)
+			throws Throwable{
+		return transaction.run();
+	}
+
+
+	default HashMap<String, String> correlate(HashMap<String, String> metadata, Message<?> handledMessage){
+		return metadata;
+	}
+}
