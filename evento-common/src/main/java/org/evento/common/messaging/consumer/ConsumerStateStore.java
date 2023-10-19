7285b99e3bd49c5bcf0e67c82dd04e15e26d1baf
Gabor Galazzo <gabor.galazzo@gmail.com>
2023-10-19 19:00:00 +0200

    Performance send queue fix time calc

--- evento-common/src/main/java/org/evento/common/messaging/consumer/ConsumerStateStore.java
+++ evento-common/src/main/java/org/evento/common/messaging/consumer/ConsumerStateStore.java
@@ -16,149 +16,144 @@ import java.util.concurrent.atomic.AtomicReference;
 
 public abstract class ConsumerStateStore {
 
-	protected final MessageBus messageBus;
-	protected final String serverNodeName;
-	private final Logger logger = LogManager.getLogger(ConsumerStateStore.class);
-	private final PerformanceService performanceService;
-	private final String bundleId;
-	private final ObjectMapper objectMapper;
-	private final AddressPicker<?> addressPicker;
+    protected final MessageBus messageBus;
+    protected final String serverNodeName;
+    private final Logger logger = LogManager.getLogger(ConsumerStateStore.class);
+    private final PerformanceService performanceService;
+    private final String bundleId;
+    private final ObjectMapper objectMapper;
+    private final AddressPicker<?> addressPicker;
 
-	protected ConsumerStateStore(
-			MessageBus messageBus,
-			String bundleId,
-			String serverNodeName,
-			PerformanceService performanceService) {
-		this.messageBus = messageBus;
-		this.serverNodeName = serverNodeName;
-		this.bundleId = bundleId;
-		this.performanceService = performanceService;
-		this.addressPicker = new RoundRobinAddressPicker(messageBus);
-		this.objectMapper = ObjectMapperUtils.getPayloadObjectMapper();
-	}
+    protected ConsumerStateStore(
+            MessageBus messageBus,
+            String bundleId,
+            String serverNodeName,
+            PerformanceService performanceService) {
+        this.messageBus = messageBus;
+        this.serverNodeName = serverNodeName;
+        this.bundleId = bundleId;
+        this.performanceService = performanceService;
+        this.addressPicker = new RoundRobinAddressPicker(messageBus);
+        this.objectMapper = ObjectMapperUtils.getPayloadObjectMapper();
+    }
 
-	public int consumeEventsForProjector(
-			String consumerId,
-			String projectorName,
-			String context,
-			ProjectorEventConsumer projectorEventConsumer,
-			int fetchSize) throws Throwable {
-		var consumedEventCount = 0;
-		if (enterExclusiveZone(consumerId))
-		{
-			try
-			{
-				var lastEventSequenceNumber = getLastEventSequenceNumber(consumerId);
-				if (lastEventSequenceNumber == null) lastEventSequenceNumber = 0L;
-				var resp = ((EventFetchResponse) messageBus.request(addressPicker.pickNodeAddress(serverNodeName),
-						new EventFetchRequest(
-								context,
-								lastEventSequenceNumber,
-								fetchSize,
-								projectorName)).get());
-				for (PublishedEvent event : resp.getEvents())
-				{
-					var start = Instant.now();
-					projectorEventConsumer.consume(event);
-					setLastEventSequenceNumber(consumerId, event.getEventSequenceNumber());
-					consumedEventCount++;
-					performanceService.sendServiceTimeMetric(
-							bundleId,
-							projectorName,
-							event.getEventMessage(),
-							start
-					);
-				}
-			} finally
-			{
-				leaveExclusiveZone(consumerId);
-			}
-		} else
-		{
-			return -1;
-		}
-		return consumedEventCount;
+    public int consumeEventsForProjector(
+            String consumerId,
+            String projectorName,
+            String context,
+            ProjectorEventConsumer projectorEventConsumer,
+            int fetchSize) throws Throwable {
+        var consumedEventCount = 0;
+        if (enterExclusiveZone(consumerId)) {
+            try {
+                var lastEventSequenceNumber = getLastEventSequenceNumber(consumerId);
+                if (lastEventSequenceNumber == null) lastEventSequenceNumber = 0L;
+                var resp = ((EventFetchResponse) messageBus.request(addressPicker.pickNodeAddress(serverNodeName),
+                        new EventFetchRequest(
+                                context,
+                                lastEventSequenceNumber,
+                                fetchSize,
+                                projectorName)).get());
+                for (PublishedEvent event : resp.getEvents()) {
+                    var start = Instant.now();
+                    try {
+                        projectorEventConsumer.consume(event);
+                    } catch (Exception e) {
+                        throw new RuntimeException("Event consumption Error for projection %s and event %s".formatted(projectorName, event.getEventName()), e);
+                    }
+                    setLastEventSequenceNumber(consumerId, event.getEventSequenceNumber());
+                    consumedEventCount++;
+                    performanceService.sendServiceTimeMetric(
+                            bundleId,
+                            projectorName,
+                            event.getEventMessage(),
+                            start
+                    );
+                }
+            } finally {
+                leaveExclusiveZone(consumerId);
+            }
+        } else {
+            return -1;
+        }
+        return consumedEventCount;
 
-	}
+    }
 
-	public int consumeEventsForSaga(String consumerId, String sagaName,
-									String context,
-									SagaEventConsumer sagaEventConsumer,
-									int fetchSize) throws Throwable {
-		var consumedEventCount = 0;
-		if (enterExclusiveZone(consumerId))
-		{
-			try
-			{
-				var lastEventSequenceNumber = getLastEventSequenceNumberSagaOrHead(consumerId);
-				var resp = ((EventFetchResponse) messageBus.request(addressPicker.pickNodeAddress(serverNodeName),
-						new EventFetchRequest(context, lastEventSequenceNumber, fetchSize, sagaName)).get());
-				for (PublishedEvent event : resp.getEvents())
-				{
-					var start = Instant.now();
-					var sagaStateId = new AtomicReference<Long>();
-					var newState = sagaEventConsumer.consume((name, associationProperty, associationValue) -> {
-						var state = getSagaState(name, associationProperty, associationValue);
-						sagaStateId.set(state.getId());
-						return state.getState();
-					}, event);
-					if (newState != null)
-					{
-						if (newState.isEnded())
-						{
-							removeSagaState(sagaStateId.get());
-						} else
-						{
-							setSagaState(sagaStateId.get(), sagaName, newState);
+    public int consumeEventsForSaga(String consumerId, String sagaName,
+                                    String context,
+                                    SagaEventConsumer sagaEventConsumer,
+                                    int fetchSize) throws Throwable {
+        var consumedEventCount = 0;
+        if (enterExclusiveZone(consumerId)) {
+            try {
+                var lastEventSequenceNumber = getLastEventSequenceNumberSagaOrHead(consumerId);
+                var resp = ((EventFetchResponse) messageBus.request(addressPicker.pickNodeAddress(serverNodeName),
+                        new EventFetchRequest(context, lastEventSequenceNumber, fetchSize, sagaName)).get());
+                for (PublishedEvent event : resp.getEvents()) {
+                    var start = Instant.now();
+                    var sagaStateId = new AtomicReference<Long>();
+                    try {
+                        var newState = sagaEventConsumer.consume((name, associationProperty, associationValue) -> {
+                            var state = getSagaState(name, associationProperty, associationValue);
+                            sagaStateId.set(state.getId());
+                            return state.getState();
+                        }, event);
+						if (newState != null) {
+							if (newState.isEnded()) {
+								removeSagaState(sagaStateId.get());
+							} else {
+								setSagaState(sagaStateId.get(), sagaName, newState);
+							}
 						}
-					}
-					setLastEventSequenceNumber(consumerId, event.getEventSequenceNumber());
-					consumedEventCount++;
-					performanceService.sendServiceTimeMetric(
-							bundleId,
-							sagaName,
-							event.getEventMessage(),
-							start
-					);
-				}
-			} finally
-			{
-				leaveExclusiveZone(consumerId);
-			}
-		} else
-		{
-			return -1;
-		}
-		return consumedEventCount;
-	}
+                    } catch (Exception e) {
+                        throw new RuntimeException("Event consumption Error for saga %s and event %s".formatted(sagaName, event.getEventName()), e);
+                    }
+
+                    setLastEventSequenceNumber(consumerId, event.getEventSequenceNumber());
+                    consumedEventCount++;
+                    performanceService.sendServiceTimeMetric(
+                            bundleId,
+                            sagaName,
+                            event.getEventMessage(),
+                            start
+                    );
+                }
+            } finally {
+                leaveExclusiveZone(consumerId);
+            }
+        } else {
+            return -1;
+        }
+        return consumedEventCount;
+    }
 
 
-	protected long getLastEventSequenceNumberSagaOrHead(String consumerId) throws Exception {
-		var last = getLastEventSequenceNumber(consumerId);
-		if (last == null)
-		{
-			var head = ((EventLastSequenceNumberResponse) this.messageBus.request(addressPicker.pickNodeAddress(serverNodeName), new EventLastSequenceNumberRequest()).get()).getNumber();
-			setLastEventSequenceNumber(consumerId, head);
-			return head;
-		}
-		return last;
-	}
+    protected long getLastEventSequenceNumberSagaOrHead(String consumerId) throws Exception {
+        var last = getLastEventSequenceNumber(consumerId);
+        if (last == null) {
+            var head = ((EventLastSequenceNumberResponse) this.messageBus.request(addressPicker.pickNodeAddress(serverNodeName), new EventLastSequenceNumberRequest()).get()).getNumber();
+            setLastEventSequenceNumber(consumerId, head);
+            return head;
+        }
+        return last;
+    }
 
-	protected abstract void removeSagaState(Long sagaId) throws Exception;
+    protected abstract void removeSagaState(Long sagaId) throws Exception;
 
-	protected abstract void leaveExclusiveZone(String consumerId) throws Exception;
+    protected abstract void leaveExclusiveZone(String consumerId) throws Exception;
 
-	protected abstract boolean enterExclusiveZone(String consumerId) throws Exception;
+    protected abstract boolean enterExclusiveZone(String consumerId) throws Exception;
 
-	protected abstract Long getLastEventSequenceNumber(String consumerId) throws Exception;
+    protected abstract Long getLastEventSequenceNumber(String consumerId) throws Exception;
 
-	protected abstract void setLastEventSequenceNumber(String consumerId, Long eventSequenceNumber) throws Exception;
+    protected abstract void setLastEventSequenceNumber(String consumerId, Long eventSequenceNumber) throws Exception;
 
-	protected abstract StoredSagaState getSagaState(String sagaName, String associationProperty, String associationValue) throws Exception;
+    protected abstract StoredSagaState getSagaState(String sagaName, String associationProperty, String associationValue) throws Exception;
 
-	protected abstract void setSagaState(Long sagaId, String sagaName, SagaState sagaState) throws Exception;
+    protected abstract void setSagaState(Long sagaId, String sagaName, SagaState sagaState) throws Exception;
 
-	protected ObjectMapper getObjectMapper() {
-		return objectMapper;
-	}
+    protected ObjectMapper getObjectMapper() {
+        return objectMapper;
+    }
 }
