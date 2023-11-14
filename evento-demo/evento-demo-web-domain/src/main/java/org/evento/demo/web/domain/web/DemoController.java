5b1a5f108bb63f3bbfed8152ec469e19c4fa47f6
Gabor Galazzo <gabor.galazzo@gmail.com>
2023-11-14 20:59:22 +0100

    Connection loss management for correlated massages

--- evento-demo/evento-demo-web-domain/src/main/java/org/evento/demo/web/domain/web/DemoController.java
+++ evento-demo/evento-demo-web-domain/src/main/java/org/evento/demo/web/domain/web/DemoController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/api/demo")
