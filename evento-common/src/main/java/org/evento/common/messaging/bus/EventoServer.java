b2627187c6bfbaa51021d704d52a171ec7d00169
Gabor Galazzo <gabor.galazzo@gmail.com>
2023-11-17 16:19:11 +0100

    minor fixes

--- evento-common/src/main/java/org/evento/common/messaging/bus/EventoServer.java
+++ evento-common/src/main/java/org/evento/common/messaging/bus/EventoServer.java
@@ -5,9 +5,9 @@ import java.util.concurrent.CompletableFuture;
 
 public interface EventoServer {
 
-    public void send(Serializable message) throws  SendFailedException;
+    void send(Serializable message) throws  SendFailedException;
 
-    public <T extends Serializable> CompletableFuture<T> request(Serializable request) throws SendFailedException;
+    <T extends Serializable> CompletableFuture<T> request(Serializable request) throws SendFailedException;
 
     String getInstanceId();
 
--- evento-parser/src/main/java/org/evento/parser/BundleParser.java
+++ evento-parser/src/main/java/org/evento/parser/BundleParser.java
@@ -5,5 +5,5 @@ import org.evento.parser.model.BundleDescription;
 import java.io.File;
 
 public interface BundleParser {
-	public BundleDescription parseDirectory(File file, String repositoryRoot) throws Exception;
+	BundleDescription parseDirectory(File file, String repositoryRoot) throws Exception;
 }
