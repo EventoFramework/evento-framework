113bb224ddf0d3d91466017e86d7dc8269668ab7
Gabor Galazzo <gabor.galazzo@gmail.com>
2024-01-21 22:54:38 +0100

    Refactor GatewayTelemetryProxy methods for cleaner code
    The code in several GatewayTelemetryProxy methods has been refactored to improve clarity and readability. Mainly, updates on command and query invocation counters are now handled within CompletableFuture's whenComplete method. This refactoring eliminates repetitive code and enhances the readability of asynchronous send and query execution methods.

--- evento-bundle/src/main/java/org/evento/application/proxy/GatewayTelemetryProxy.java
+++ evento-bundle/src/main/java/org/evento/application/proxy/GatewayTelemetryProxy.java
@@ -23,187 +23,172 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public class GatewayTelemetryProxy implements CommandGateway, QueryGateway {
 
-	private final CommandGateway commandGateway;
-	private final QueryGateway queryGateway;
+    private final CommandGateway commandGateway;
+    private final QueryGateway queryGateway;
 
-	private final String bundle;
-	private final String componentName;
+    private final String bundle;
+    private final String componentName;
 
-	private final PerformanceService performanceService;
+    private final PerformanceService performanceService;
 
-	private final HashMap<String, AtomicInteger> invocationCounter = new HashMap<>();
+    private final HashMap<String, AtomicInteger> invocationCounter = new HashMap<>();
 
-	private final Message<?> handledMessage;
-	private final TracingAgent tracingAgent;
+    private final Message<?> handledMessage;
+    private final TracingAgent tracingAgent;
 
-	/**
-	 * Constructs a new GatewayTelemetryProxy with the specified parameters.
-	 *
-	 * @param commandGateway     The command gateway to proxy.
-	 * @param queryGateway       The query gateway to proxy.
-	 * @param bundle             The bundle identifier.
-	 * @param performanceService The performance service for tracking metrics.
-	 * @param componentName      The name of the component associated with the proxy.
-	 * @param handledMessage     The message being handled by the proxy.
-	 * @param tracingAgent       The tracing agent for correlating and tracking.
-	 */
-	public GatewayTelemetryProxy(CommandGateway commandGateway,
-								 QueryGateway queryGateway,
-								 String bundle, PerformanceService performanceService,
-								 String componentName,
-								 Message<?> handledMessage,
-								 TracingAgent tracingAgent) {
-		this.commandGateway = commandGateway;
-		this.queryGateway = queryGateway;
-		this.bundle = bundle;
-		this.performanceService = performanceService;
-		this.componentName = componentName;
-		this.handledMessage = handledMessage;
-		this.tracingAgent = tracingAgent;
-	}
+    /**
+     * Constructs a new GatewayTelemetryProxy with the specified parameters.
+     *
+     * @param commandGateway     The command gateway to proxy.
+     * @param queryGateway       The query gateway to proxy.
+     * @param bundle             The bundle identifier.
+     * @param performanceService The performance service for tracking metrics.
+     * @param componentName      The name of the component associated with the proxy.
+     * @param handledMessage     The message being handled by the proxy.
+     * @param tracingAgent       The tracing agent for correlating and tracking.
+     */
+    public GatewayTelemetryProxy(CommandGateway commandGateway,
+                                 QueryGateway queryGateway,
+                                 String bundle, PerformanceService performanceService,
+                                 String componentName,
+                                 Message<?> handledMessage,
+                                 TracingAgent tracingAgent) {
+        this.commandGateway = commandGateway;
+        this.queryGateway = queryGateway;
+        this.bundle = bundle;
+        this.performanceService = performanceService;
+        this.componentName = componentName;
+        this.handledMessage = handledMessage;
+        this.tracingAgent = tracingAgent;
+    }
 
-	@Override
-	public <R> R sendAndWait(Command command) {
-		try {
-			return commandGateway.sendAndWait(command, tracingAgent.correlate((Metadata) null, this.handledMessage));
-		} finally {
-			updateInvocationCounter(command);
-		}
-	}
+    @Override
+    public <R> R sendAndWait(Command command) {
+        try {
+            return commandGateway.sendAndWait(command, tracingAgent.correlate((Metadata) null, this.handledMessage));
+        } finally {
+            updateInvocationCounter(command);
+        }
+    }
 
-	@Override
-	public <R> R sendAndWait(Command command, long timeout, TimeUnit unit) {
-		try {
-			return commandGateway.sendAndWait(command, tracingAgent.correlate((Metadata) null, this.handledMessage), timeout, unit);
-		} finally {
-			updateInvocationCounter(command);
-		}
-	}
+    @Override
+    public <R> R sendAndWait(Command command, long timeout, TimeUnit unit) {
+        try {
+            return commandGateway.sendAndWait(command, tracingAgent.correlate((Metadata) null, this.handledMessage), timeout, unit);
+        } finally {
+            updateInvocationCounter(command);
+        }
+    }
 
-	@Override
-	public <R> CompletableFuture<R> send(Command command) {
-		try {
-			return commandGateway.send(command, tracingAgent.correlate((Metadata) null, this.handledMessage));
-		} finally {
-			updateInvocationCounter(command);
-		}
-	}
+    @Override
+    @SuppressWarnings("unchecked")
+    public <R> CompletableFuture<R> send(Command command) {
+        return (CompletableFuture<R>) commandGateway.send(command, tracingAgent.correlate((Metadata) null, this.handledMessage))
+                .whenComplete((s, e) -> updateInvocationCounter(command));
+    }
 
-	@Override
-	public <R> R sendAndWait(Command command, Metadata metadata) {
-		try {
-			return commandGateway.sendAndWait(command, tracingAgent.correlate(metadata, this.handledMessage));
-		} finally {
-			updateInvocationCounter(command);
-		}
-	}
+    @Override
+    public <R> R sendAndWait(Command command, Metadata metadata) {
+        try {
+            return commandGateway.sendAndWait(command, tracingAgent.correlate(metadata, this.handledMessage));
+        } finally {
+            updateInvocationCounter(command);
+        }
+    }
 
-	@Override
-	public <R> R sendAndWait(Command command, Metadata metadata, long timeout, TimeUnit unit) {
-		try {
-			return commandGateway.sendAndWait(command, tracingAgent.correlate(metadata, this.handledMessage), timeout, unit);
-		} finally {
-			updateInvocationCounter(command);
-		}
-	}
+    @Override
+    public <R> R sendAndWait(Command command, Metadata metadata, long timeout, TimeUnit unit) {
+        try {
+            return commandGateway.sendAndWait(command, tracingAgent.correlate(metadata, this.handledMessage), timeout, unit);
+        } finally {
+            updateInvocationCounter(command);
+        }
+    }
 
-	@Override
-	public <R> CompletableFuture<R> send(Command command, Metadata metadata) {
-		try {
-			return commandGateway.send(command,  tracingAgent.correlate(metadata, this.handledMessage));
-		} finally {
-			updateInvocationCounter(command);
-		}
-	}
+    @Override
+    @SuppressWarnings("unchecked")
+    public <R> CompletableFuture<R> send(Command command, Metadata metadata) {
+        return (CompletableFuture<R>) commandGateway.send(command, tracingAgent.correlate(metadata, this.handledMessage))
+                .whenComplete((s, e) -> updateInvocationCounter(command));
+    }
 
-	@Override
-	public <R> R sendAndWait(Command command, Metadata metadata, Message<?> handledMessage) {
-		try {
-			return commandGateway.sendAndWait(command, tracingAgent.correlate(metadata, this.handledMessage), this.handledMessage);
-		} finally {
-			updateInvocationCounter(command);
-		}
-	}
+    @Override
+    public <R> R sendAndWait(Command command, Metadata metadata, Message<?> handledMessage) {
+        try {
+            return commandGateway.sendAndWait(command, tracingAgent.correlate(metadata, this.handledMessage), this.handledMessage);
+        } finally {
+            updateInvocationCounter(command);
+        }
+    }
 
-	@Override
-	public <R> R sendAndWait(Command command, Metadata metadata, Message<?> handledMessage, long timeout, TimeUnit unit) {
-		try {
-			return commandGateway.sendAndWait(command, tracingAgent.correlate(metadata, this.handledMessage), this.handledMessage, timeout, unit);
-		} finally {
-			updateInvocationCounter(command);
-		}
-	}
+    @Override
+    public <R> R sendAndWait(Command command, Metadata metadata, Message<?> handledMessage, long timeout, TimeUnit unit) {
+        try {
+            return commandGateway.sendAndWait(command, tracingAgent.correlate(metadata, this.handledMessage), this.handledMessage, timeout, unit);
+        } finally {
+            updateInvocationCounter(command);
+        }
+    }
 
-	@Override
-	public <R> CompletableFuture<R> send(Command command, Metadata metadata, Message<?> handledMessage) {
-		try {
-			return commandGateway.send(command, tracingAgent.correlate(metadata, this.handledMessage), this.handledMessage);
-		} finally {
-			updateInvocationCounter(command);
-		}
-	}
+    @Override
+    @SuppressWarnings("unchecked")
+    public <R> CompletableFuture<R> send(Command command, Metadata metadata, Message<?> handledMessage) {
+        return (CompletableFuture<R>) commandGateway.send(command, tracingAgent.correlate(metadata, this.handledMessage),
+                this.handledMessage).whenComplete((s, e) -> updateInvocationCounter(command));
+    }
 
-	@Override
-	public <T extends QueryResponse<?>> CompletableFuture<T> query(Query<T> query, Metadata metadata, Message<?> handledMessage) {
-		try {
-			return queryGateway.query(query, tracingAgent.correlate(metadata, this.handledMessage), this.handledMessage);
-		} finally {
-			updateInvocationCounter(query);
-		}
-	}
+    @Override
+    public <T extends QueryResponse<?>> CompletableFuture<T> query(Query<T> query, Metadata metadata, Message<?> handledMessage) {
+        return queryGateway.query(query, tracingAgent.correlate(metadata, this.handledMessage), this.handledMessage)
+                .whenComplete((s, e) -> updateInvocationCounter(query));
+    }
 
-	@Override
-	public <T extends QueryResponse<?>> CompletableFuture<T> query(Query<T> query) {
-		try {
-			return queryGateway.query(query, tracingAgent.correlate((Metadata) null, this.handledMessage));
-		} finally {
-			updateInvocationCounter(query);
-		}
-	}
+    @Override
+    public <T extends QueryResponse<?>> CompletableFuture<T> query(Query<T> query) {
+        return queryGateway.query(query, tracingAgent.correlate((Metadata) null, this.handledMessage))
+                .whenComplete((s, e) -> updateInvocationCounter(query));
+    }
 
-	@Override
-	public <T extends QueryResponse<?>> CompletableFuture<T> query(Query<T> query, Metadata metadata) {
-		try {
-			return queryGateway.query(query, tracingAgent.correlate(metadata, this.handledMessage));
-		} finally {
-			updateInvocationCounter(query);
-		}
-	}
+    @Override
+    public <T extends QueryResponse<?>> CompletableFuture<T> query(Query<T> query, Metadata metadata) {
+        return queryGateway.query(query, tracingAgent.correlate(metadata, this.handledMessage))
+                .whenComplete((s, e) -> updateInvocationCounter(query));
+    }
 
-	/**
-	 * Updates the invocation counter for a specific command or query.
-	 *
-	 * @param message The command or query being executed.
-	 */
-	private void updateInvocationCounter(Payload message) {
-		invocationCounter.putIfAbsent(message.getClass().getSimpleName(), new AtomicInteger());
-		invocationCounter.get(message.getClass().getSimpleName()).incrementAndGet();
-	}
+    /**
+     * Updates the invocation counter for a specific command or query.
+     *
+     * @param message The command or query being executed.
+     */
+    private void updateInvocationCounter(Payload message) {
+        invocationCounter.putIfAbsent(message.getClass().getSimpleName(), new AtomicInteger());
+        invocationCounter.get(message.getClass().getSimpleName()).incrementAndGet();
+    }
 
-	/**
-	 * Sends the invocations metric to the performance service.
-	 */
-	public void sendInvocationsMetric() {
-		performanceService.sendInvocationsMetric(
-				bundle,
-				componentName,
-				handledMessage,
-				invocationCounter
-		);
-	}
+    /**
+     * Sends the invocations metric to the performance service.
+     */
+    public void sendInvocationsMetric() {
+        performanceService.sendInvocationsMetric(
+                bundle,
+                componentName,
+                handledMessage,
+                invocationCounter
+        );
+    }
 
-	/**
-	 * Sends the service time metric to the performance service and then sends the invocations metric.
-	 *
-	 * @param start The start time of the service operation.
-	 */
-	public void sendServiceTimeMetric(Instant start) {
-		performanceService.sendServiceTimeMetric(
-				bundle,
-				componentName,
-				handledMessage,
-				start
-		);
-		sendInvocationsMetric();
-	}
+    /**
+     * Sends the service time metric to the performance service and then sends the invocations metric.
+     *
+     * @param start The start time of the service operation.
+     */
+    public void sendServiceTimeMetric(Instant start) {
+        performanceService.sendServiceTimeMetric(
+                bundle,
+                componentName,
+                handledMessage,
+                start
+        );
+        sendInvocationsMetric();
+    }
 }
