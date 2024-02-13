f31e97cbd6da71629c4660eb2d1bf538544349a8
Gabor Galazzo <gabor.galazzo@gmail.com>
2024-02-13 22:45:58 +0100

    Add lock and aggregate id to Command messages, upgrade to version ev1.7.0
    Added a method to retrieve lock and aggregate ids in Command and ServiceCommand. Upgraded application versions to ev1.7.0 in gradle build files and application properties. Code refactoring was done in MessageBus class to handle these changes effectively.

--- evento-common/src/main/java/com/evento/common/modeling/messaging/message/application/CommandMessage.java
+++ evento-common/src/main/java/com/evento/common/modeling/messaging/message/application/CommandMessage.java
@@ -12,6 +12,9 @@ import com.evento.common.modeling.messaging.payload.Command;
  */
 public abstract class CommandMessage<T extends Command> extends Message<T> {
 
+	private String aggregateId;
+	private String lockId;
+
 	/**
 	 * CommandMessage is a subclass of Message that represents a message carrying a command payload.
 	 * It is meant to be subclassed for specific types of commands.
@@ -20,6 +23,8 @@ public abstract class CommandMessage<T extends Command> extends Message<T> {
      */
 	public CommandMessage(T command) {
 		super(command);
+		this.aggregateId = command.getAggregateId();
+		this.lockId = command.getLockId();
 	}
 
 	/**
@@ -39,5 +44,50 @@ public abstract class CommandMessage<T extends Command> extends Message<T> {
 	 */
 	public String getCommandName() {
 		return super.getPayloadName();
+	}
+
+	/**
+	 * Retrieves the ID of the aggregate the command is targeting.
+	 *
+	 * @return The ID of the aggregate.
+	 */
+	public String getAggregateId() {
+		return aggregateId;
+	}
+
+	/**
+	 * Sets the ID of the aggregate that the command is targeting.
+	 *
+	 * @param aggregateId The ID of the aggregate as a string.
+	 */
+	public void setAggregateId(String aggregateId) {
+		this.aggregateId = aggregateId;
+	}
+
+
+	/**
+	 * Sets the lock ID for the ServiceCommandMessage.
+	 *
+	 * @param lockId The lock ID to be set.
+	 */
+	public void setLockId(String lockId) {
+		this.lockId = lockId;
+	}
+
+	/**
+	 * Retrieves the lock ID associated with the ServiceCommandMessage.
+	 *
+	 * @return The lock ID associated with the ServiceCommandMessage.
+	 */
+	public String getLockId() {
+		return lockId;
+	}
+
+
+	@Override
+	public void setPayload(T payload) {
+		super.setPayload(payload);
+		this.lockId = payload.getLockId();
+		this.aggregateId = payload.getAggregateId();
 	}
 }
--- evento-common/src/main/java/com/evento/common/modeling/messaging/payload/Command.java
+++ evento-common/src/main/java/com/evento/common/modeling/messaging/payload/Command.java
@@ -6,5 +6,19 @@ package com.evento.common.modeling.messaging.payload;
  * It extends the Payload interface, which represents a payload object that can be used in a software system.
  */
 public interface Command extends Payload {
+    /**
+     * Retrieves the ID of the aggregate that the command is targeting.
+     *
+     * @return The aggregate ID as a string.
+     */
+    String getAggregateId();
+
+    /**
+     * Retrieves the lock ID associated with the ServiceCommand.
+     *
+     * @return The lock ID associated with the ServiceCommand.
+     */
+    @SuppressWarnings("SameReturnValue")
+    String getLockId();
 
 }
