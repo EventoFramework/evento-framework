0444bedde217cf40e9234f8819176a8b6460cfae
Gabor Galazzo <gabor.galazzo@gmail.com>
2024-08-19 12:34:27 +0200

    Update UI and payload classes for enhanced clarity
    Updated consumers component to include component version in the UI for better identification. Modified Command and Query classes to extend TrackablePayload, providing more specific documentation and method details.

--- evento-common/src/main/java/com/evento/common/modeling/messaging/payload/Command.java
+++ evento-common/src/main/java/com/evento/common/modeling/messaging/payload/Command.java
@@ -2,8 +2,9 @@ package com.evento.common.modeling.messaging.payload;
 
 
 /**
- * The Command abstract class represents a command object that can be used in a software system.
- * It extends the Payload interface, which represents a payload object that can be used in a software system.
+ * The Command class is an abstract class representing a command that can be sent to a service.
+ * It extends the TrackablePayload class and defines additional methods to retrieve the ID of the
+ * aggregate entity and the lock ID associated with the command.
  */
 public abstract class Command extends TrackablePayload {
 
