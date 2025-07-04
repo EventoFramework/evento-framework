1313b70d48a2d7212f1d1357dd45b6c856ecbb4a
Gabor Galazzo <gabor.galazzo@gmail.com>
2025-07-04 22:24:12 +0200

    Update build files for Maven repository and versioning improvements
    Refactored Maven repository configuration to use `ossrh-staging-api` URL and updated credentials handling. Adjusted versioning inline comments to indicate potential '-SNAPSHOT' usage. Added a new `publish.py` script for managing Gradle properties and repository interactions. Updated `.gitignore` to exclude new entries (`maven`, `.venv`).

---
+++ publish.py
@@ -0,0 +1,34 @@
+import requests
+import os
+
+def get_gradle_properties():
+    properties = {}
+    gradle_properties_path = os.path.expanduser("~/.gradle/gradle.properties")
+
+    if not os.path.exists(gradle_properties_path):
+        print("No gradle.properties file found in ~/.gradle/")
+        return properties
+
+    with open(gradle_properties_path, 'r', encoding='utf-8') as f:
+        for line in f:
+            line = line.strip()
+            if not line or line.startswith('#'):
+                continue
+            if '=' in line:
+                key, value = line.split('=', 1)
+                properties[key.strip()] = value.strip()
+    return properties
+
+# Example usage
+if __name__ == "__main__":
+    props = get_gradle_properties()
+
+    resp = requests.get("https://ossrh-staging-api.central.sonatype.com/manual/search/repositories",
+                 auth=(props['mavenCentralUsername'], props['mavenCentralPassword']))
+
+    print(resp.text)
+
+    #resp = requests.post("https://ossrh-staging-api.central.sonatype.com/manual/upload/defaultRepository/com.eventoframework?publishing_type=user_managed",
+    #                    auth=(props['mavenCentralUsername'], props['mavenCentralPassword']))
+
+    print(resp.text)
