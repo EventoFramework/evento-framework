3b9bdf3947eaeb9b084ba981a9d5744022f9a653
Gabor Galazzo <gabor.galazzo@gmail.com>
2025-07-13 17:24:33 +0200

    Add `BundleRegistered` message type supporting bundle registration status, enhance connection logic, and refactor `ClusterConnection` for better error handling. Replace `FakeProjector` with multiple test projectors for improved event handling coverage.

---
+++ evento-demo/evento-demo-query/src/main/java/com/evento/demo/memory/query/Fake5Projector.java
@@ -0,0 +1,29 @@
+package com.evento.demo.memory.query;
+
+import com.evento.common.modeling.annotations.component.Projector;
+import com.evento.common.modeling.annotations.handler.EventHandler;
+import com.evento.demo.api.event.DemoCreatedEvent;
+import com.evento.demo.api.event.DemoDeletedEvent;
+import com.evento.demo.api.event.DemoUpdatedEvent;
+import com.evento.demo.api.utils.Utils;
+
+@Projector(version =1)
+public class Fake5Projector {
+
+    @EventHandler(retry = 3)
+    void on(DemoCreatedEvent event) {
+        Utils.logMethodFlow(this, "on", event, "HIT");
+    }
+
+    @EventHandler
+    void on(DemoUpdatedEvent event) {
+        Utils.logMethodFlow(this, "on", event, "HIT");
+
+    }
+
+    @EventHandler
+    void on(DemoDeletedEvent event) {
+        Utils.logMethodFlow(this, "on", event, "HIT");
+
+    }
+}
